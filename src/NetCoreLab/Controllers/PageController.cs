672257bd6985309c200747ba1f20772611331b81
Anders Sjöberg <shoobah@gmil.com>
2016-01-30 03:42:05 +0100

    First

---
+++ src/NetCoreLab/Controllers/PageController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreLab.Controllers
+{
+    [Route("api/[controller]")]
+    public class PageController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var startup = new Startup(new HostingEnvironment());
+            var esUrl = startup.Configuration["ElasticSearchSettings:BaseUrl"];
+            return new string[] { esUrl };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
